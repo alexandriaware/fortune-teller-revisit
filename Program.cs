e1050d44f4af379f47b3129b0b08e042f6229b65
alexandriaware <alexandrialware@gmail.com>
2019-05-21 16:51:53 -0400

    put birth month question into do while for validtion

--- Program.cs
+++ Program.cs
@@ -19,60 +19,69 @@ namespace fortune_teller_revisit
             Console.WriteLine("In which month were you born?");
             string userMonthString = Console.ReadLine().ToLower();
 
-            int userBirthMonthInt;
+            int userBirthMonthInt = 0;
+            bool isValid = true;
 
-            if(userMonthString == "january") 
+            do
             {
-                userBirthMonthInt = 1;
-            }
-            else if(userMonthString == "february")
-            {
-                userBirthMonthInt = 2;
-            }
-            else if (userMonthString == "march")
-            {
-                userBirthMonthInt = 3;
-            }
-            else if (userMonthString == "april")
-            {
-                userBirthMonthInt = 4;
-            }
-            else if (userMonthString == "may")
-            {
-                userBirthMonthInt = 5;
-            }
-            else if (userMonthString == "june")
-            {
-                userBirthMonthInt = 6;
-            }
-            else if (userMonthString == "july")
-            {
-                userBirthMonthInt = 7;
-            }
-            else if (userMonthString == "august")
-            {
-                userBirthMonthInt = 8;
-            }
-            else if (userMonthString == "september")
-            {
-                userBirthMonthInt = 9;
-            }
-            else if (userMonthString == "october")
-            {
-                userBirthMonthInt = 10;
-            }
-            else if (userMonthString == "november")
-            {
-                userBirthMonthInt = 11;
-            }
-            else if (userMonthString == "december")
-            {
-                userBirthMonthInt = 12;
-            }
-            else
-            {
-                Console.WriteLine("Invalid option. Please try again.");
-            }
+                if (userMonthString == "january")
+                {
+                    userBirthMonthInt = 1;
+                }
+                else if (userMonthString == "february")
+                {
+                    userBirthMonthInt = 2;
+                }
+                else if (userMonthString == "march")
+                {
+                    userBirthMonthInt = 3;
+                }
+                else if (userMonthString == "april")
+                {
+                    userBirthMonthInt = 4;
+                }
+                else if (userMonthString == "may")
+                {
+                    userBirthMonthInt = 5;
+                }
+                else if (userMonthString == "june")
+                {
+                    userBirthMonthInt = 6;
+                }
+                else if (userMonthString == "july")
+                {
+                    userBirthMonthInt = 7;
+                }
+                else if (userMonthString == "august")
+                {
+                    userBirthMonthInt = 8;
+                }
+                else if (userMonthString == "september")
+                {
+                    userBirthMonthInt = 9;
+                }
+                else if (userMonthString == "october")
+                {
+                    userBirthMonthInt = 10;
+                }
+                else if (userMonthString == "november")
+                {
+                    userBirthMonthInt = 11;
+                }
+                else if (userMonthString == "december")
+                {
+                    userBirthMonthInt = 12;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option. Please try again.");
+                    isValid = false;
+                }
+            } while (isValid == false);
+
+            Console.WriteLine(userBirthMonthInt);
+
+            Console.ReadKey();
         }
     }
 }
